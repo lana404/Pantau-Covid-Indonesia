b8ac7c4e9e535e90ad5755081a9b30c6bfec8d19
lana404 <ihwan.mualana@students.amikom.ac.id>
2020-05-02 18:22:18 +0700

    Update

--- commit.sh
+++ commit.sh
@@ -4,17 +4,25 @@
 #
 #########################################################
 
+
 echo "Git Username : "
 read username
 
-echo "Git Password"
+echo "Git Password : "
 read password
 
 echo "Commit Message"
 read message
+
+
+git config --global user.name $username
+
+git config --global user.password $password
 
 git init
 
 git add .
 
 git commit -m $message
+
+git push -u origin master
