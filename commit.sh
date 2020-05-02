385de6ef57a9414a06b678bc0fdb38419c78618c
Ihwan Mualana <ihwan.mualana@students.amikom.ac.id>
2020-05-02 18:15:48 +0700

    Update

--- commit.sh
+++ commit.sh
@@ -1,1 +1,20 @@
- 
+# !/bin/sh
+#
+# Copyright (C) 2019 Ihwan Mualana <ihwan.cfc@gmail.com>
+#
+#########################################################
+
+echo "Git Username : "
+read username
+
+echo "Git Password"
+read password
+
+echo "Commit Message"
+read message
+
+git init
+
+git add .
+
+git commit -m $message
